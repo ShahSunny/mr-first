1cb2d89376db5e46c43a9c0855ff466b1f72aaf9
Sunny Shah <sunny.cpp@gmail.com>
2015-03-08 20:11:53 +0530

    Added multiple path filter

--- src/main/java/org/sunny/mr/first_mr/AppRunner.java
+++ src/main/java/org/sunny/mr/first_mr/AppRunner.java
@@ -1,8 +1,14 @@
 package org.sunny.mr.first_mr;
 
+import java.io.IOException;
+
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.conf.Configured;
+import org.apache.hadoop.fs.FileStatus;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.FileUtil;
 import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.fs.PathFilter;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
@@ -40,13 +46,38 @@ public class AppRunner extends Configured implements Tool {
         //setting the output data type classes
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(IntWritable.class);
-
-        //to accept the hdfs input and outpur dir at run time
-        FileInputFormat.addInputPath(job, new Path(args[0]));
+        Path[] paths = filterOutPaths(args[0]);
+        //to accept the hdfs input and outpur dir at run time        
+        FileInputFormat.setInputPaths(job, paths);
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         boolean success = job.waitForCompletion(true);
     	System.exit(success ? 0 : 1);
 		return 0;
+	}
+
+
+	private Path[] filterOutPaths(String path) {
+		try {
+			final FileSystem fs = FileSystem.get(getConf());
+			FileStatus[] filesFound= fs.listStatus(new Path(path), new PathFilter() {
+				public boolean accept(Path path) {
+					try {
+						FileStatus fileStatus = fs.getFileStatus(path);
+						if(fileStatus.isDirectory() && path.getName().matches("^190.$")) {
+							return true;
+						} 
+					} catch(IOException e) {
+						
+					} 
+					return false;
+				}
+			});
+			return FileUtil.stat2Paths(filesFound);			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}		
+		return null;
 	}
 }
